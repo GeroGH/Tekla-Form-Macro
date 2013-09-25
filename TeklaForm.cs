bf479305555046ed53ab1fc480043b3fb607aa0a
Dale Nicholls <dlnchlls@gmail.com>
2013-09-25 14:51:13 +0100

    First Commit

---
+++ TeklaForm.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Tekla.Technology.Akit;
+using Tekla.Structures;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+
+namespace Tekla.Technology.Akit.UserScript
+{
+    public class TeklaForm : System.Windows.Forms.Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        static Tekla.Technology.Akit.IScript akit;
+
+        public TeklaForm(Tekla.Technology.Akit.IScript RunMe)
+        {
+            InitializeComponent();
+
+            akit = RunMe;
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TeklaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Name = "TeklaForm";
+            this.Text = "TeklaForm";
+            this.ResumeLayout(false);
+        }
+    }
+
+    public class Script
+    {
+        static Tekla.Technology.Akit.IScript akit;
+
+        public static void Run(Tekla.Technology.Akit.IScript RunMe)
+        {
+            akit = RunMe;
+            Application.Run(new TeklaForm(akit));
+        }
+    }
+}
